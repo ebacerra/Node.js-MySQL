733acc0a4c960a1e6a1cbf16e0e718fc615dfd4d
Ellen <ellen.bacerra@gmail.com>
2018-09-03 22:42:52 -0500

    update

--- bamazon.sql
+++ bamazon.sql
@@ -5,40 +5,40 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-  item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NULL,
-  department_name VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
-  stock_quantity INT NULL,
+  Item_ID INT (11) NOT NULL AUTO_INCREMENT,
+  Product_Name VARCHAR(45) NULL,
+  Department_Name VARCHAR(45) NULL,
+  Price DECIMAL(10,2) NULL,
+  Stock_Quantity INT (10) NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Women's Blouse", Women, 24.99, 100);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Women's Blouse", Women, 24.99, 10);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Dog Food", Pet Supplies, 46.99, 120);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Dog Food", Pet Supplies, 46.99, 6);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Cracking The Coding Interview", Computer Programming, 34.99, 75);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Cracking The Coding Interview", Computer Programming, 34.99, 5);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Colgate", Beauty & Personal Care, 3.49, 300);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Colgate", Beauty & Personal Care, 3.49, 10);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Calphalon Cookware", Home & Kitchen, 540.99, 100);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Calphalon Cookware", Home & Kitchen, 540.99, 3);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Designer Purses", Womens Fashion, 300, 100);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Designer Purses", Womens Fashion, 300, 8);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Crossfit Shoes", Womens Athletic, 64.99, 250);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Crossfit Shoes", Womens Athletic, 64.99, 9);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Alexa", Home Audio, 124.99, 100);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Alexa", Home Audio, 124.99, 3);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Mountain Bike", Outdoor Recreation, 160.99, 50);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Mountain Bike", Outdoor Recreation, 160.99, 7);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Scented Candles", Beauty & Personal Care, 28.79, 200);
+INSERT INTO products (Product_Name, Department_Name, Price, Stock_Quantity)
+VALUES ("Scented Candles", Beauty & Personal Care, 28.79, 9);
